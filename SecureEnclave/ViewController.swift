a6c175b5ca6816eb6ff7496b4712d6dcc87cab47
alexchen <AlexChen7022@gmail.com>
2023-10-21 20:00:52 +0800

    Initial Commit

---
+++ SecureEnclave/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SecureEnclave
+//
+//  Created by 涼麵 on 2023/10/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
